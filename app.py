6c5ad06265b1232f0bb1579eeeca869cc0fd6715
Nang mon kham <103887616+monkham21@users.noreply.github.com>
2025-04-20 14:21:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ Created on Sun Apr 20 13:52:52 2025
 """
 #import libraries
 import streamlit as st
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, decode_predictions, preprocess_input
-from tensorflow.keras.preprocessing import image
+
 import numpy as np
 from PIL import Image
 import pickle
